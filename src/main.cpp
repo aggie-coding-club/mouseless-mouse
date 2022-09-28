bb61ca85e0074a8c21b879369b543a4e9f2cab35
aj4779 <aj4779@tamu.edu>
2022-09-27 19:05:19 -0500

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(12,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWRITE(12,HIGH);
 }
