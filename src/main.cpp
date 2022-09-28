7a9a46c3de511ce8e614e7f36ee4642d20a3475f
Joshua Perry <josh.m.perry@gmail.com>
2022-09-27 19:14:59 -0500

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-import time;
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(12,OUTPUT);
@@ -10,7 +8,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(12,HIGH);
-  sleep(0.5);
+  delay(0.5);
   digitalWrite(12,LOW);
-  sleep(0.5);
+  delay(0.5);
 }
