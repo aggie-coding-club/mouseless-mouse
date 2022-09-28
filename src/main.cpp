50b284530f488cbf870375a30933c29919815d16
Joshua Perry <josh.m.perry@gmail.com>
2022-09-27 19:13:20 -0500

    modified:   .pio/build/project.checksum 	deleted:    .pio/build/wemos_d1_uno32/idedata.json 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+import time;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(12,OUTPUT);
@@ -8,4 +10,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(12,HIGH);
+  sleep(0.5);
+  digitalWrite(12,LOW);
+  sleep(0.5);
 }
