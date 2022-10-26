ac7b3cc9915ce1c49b45407f5b65c02e25de4daf
euryjoshuad <euryjoshuad@tamu.edu>
2022-10-26 18:27:00 -0500

    modified:   .pio/build/project.checksum modified:   .vscode/c_cpp_properties.json modified:   platformio.ini modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(12,OUTPUT);
+  Serial.begin(115200);
+  delay(100);
+  if (!mpu.begin()) {
+    Serial.println("Could not find MPU");
+    while(1);
+  }
+  Serial.println("Found MPU6050");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(12,HIGH);
-  delay(1000);
-  digitalWrite(12,LOW);
-  delay(1000);
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  Serial.printf("Rotation: %f, %f, %f\n", g.gyro.x, g.gyro.y, g.gyro.z);
+  delay(500);
 }
