6144c282f2a294a79ee6d412fc33b6b1b62e51a3
euryjoshuad <euryjoshuad@tamu.edu>
2022-11-02 19:04:16 -0500

    modified:   .pio/build/project.checksum 	modified:   .vscode/c_cpp_properties.json 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <Arduino.h>
 #include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
+
+#define BUTTON_PIN 14
 
 Adafruit_MPU6050 mpu;
 
+volatile bool printMotion = false;
+
+void IRAM_ATTR getMPU() {
+  printMotion = true;
+}
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(BUTTON_PIN, getMPU, FALLING);
   Serial.begin(115200);
   delay(100);
   if (!mpu.begin()) {
@@ -20,6 +29,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
+  if (printMotion) {
+    Serial.println("Button pressed!");
+    printMotion = false;
+  }
   Serial.printf("Rotation: %f, %f, %f\n", g.gyro.x, g.gyro.y, g.gyro.z);
-  delay(500);
+  delay(50);
 }
