8af931ad4e87fca45de0fc48964b8b4363ae4b28
JoshmPerry <108098612+JoshmPerry@users.noreply.github.com>
2023-02-27 22:10:40 -0600

    Revert "	modified:   .pio/build/project.checksum"
    This reverts commit c4dcc65a22e041ec79069f1fc70f333c7af1554b.
    
    	.pio/build/project.checksum
    	.vscode/c_cpp_properties.json
    
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 #include <Wire.h>
 #include <BleMouse.h>
 
-#define BUTTON_PIN 14 //Which pin is the mouse click button connected to?
-#define AVG_SIZE 20 //How many inputs will we keep in rolling average array?
+#define BUTTON_PIN 14
+#define AVG_SIZE 20
 
-Adafruit_MPU6050 mpu; //Initialize MPU - tracks rotation in terms of acceleratioin + gyroscope
-BleMouse mouse("Mouseless Mouse", "Espressif", 100); //Initialize bluetooth mouse to send mouse events
+Adafruit_MPU6050 mpu;
+BleMouse mouse("Mouseless Mouse", "Espressif", 100);
 
 
 inline int sign(float inVal) {
@@ -16,7 +16,7 @@ inline int sign(float inVal) {
 }
 
 class RollingAverage {
-/*Used to keep track of past inputed values to try to smooth movement*/
+
 private:
   float avg[AVG_SIZE];
   bool isInit = false;  // Is the avg buffer full of samples?
@@ -29,7 +29,6 @@ public:
   ~RollingAverage () {}
 
   void update(float val) {
-    /*Update Rolling array with input*/
     if (isInit) {
       this->avg[++this->head%=AVG_SIZE] = val;
       float sum = 0;
@@ -49,30 +48,24 @@ public:
   }
 
   uint8_t stability() {
-    /*Returns how consistant is the recent input with eachother?*/
     return this->dStable;
   }
 
   float get() {
-    /*Returns the average value of recent inputs*/
     return this->avgVal;
   }
 };
 
-volatile bool buttonPress = false;//Is the button currently pressed?
+volatile bool buttonPress = false;
 
-//Initialize values to be smoothed later on
 RollingAverage roll;
 RollingAverage pitch;
 RollingAverage gRoll;
 RollingAverage gPitch;
 
-
-//set previous inputs to 0
 float oldRoll = 0;
 float oldPitch = 0;
 
-//sets up a function that can interupt and immediately be ran
 void IRAM_ATTR onButtonPress() {
   buttonPress = true;
 }
@@ -80,31 +73,21 @@ void IRAM_ATTR onButtonPress() {
 void setup() {
   // put your setup code here, to run once:
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(BUTTON_PIN, onButtonPress, FALLING);//when Button_pin goes from High to low (button is released) onButtonPress is ran
-
-  //starts Serial Monitor
+  attachInterrupt(BUTTON_PIN, onButtonPress, FALLING);
   Serial.begin(115200);
-
-
-  if (!mpu.begin()) {//if MPU can't connect
+  if (!mpu.begin()) {
     Serial.println("Could not find MPU");
     while(1);
   }
-
   Serial.println("Found MPU6050");
   Serial.println("Starting Bluetooth mouse");
-  mouse.begin();//starts Bluetooth mouse
+  mouse.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-
-  sensors_event_t a, g, temp;//sets events (special class)
+  sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-
-
-  //button press stuff
   if (buttonPress) {
     if (mouse.isConnected()) {
       mouse.click();
@@ -112,30 +95,19 @@ void loop() {
     else Serial.println("Mouse not connected!");
     buttonPress = false;
   }
-
-
-  roll.update(-atan2(a.acceleration.x, a.acceleration.z));//weird math to calculate direction change for roll (currently x-axis) then add it to array of prior inputs
+  roll.update(-atan2(a.acceleration.x, a.acceleration.z));
   pitch.update(-atan2(a.acceleration.y,a.acceleration.z));
   gRoll.update(g.gyro.y);
-  gPitch.update(g.gyro.x);//adding gyroscope in x axis
-
-  //currently using information from gyroscope for if statements
-  if (gPitch.stability() > 8) {//if the stability is decent
-
-    if (mouse.isConnected()){//If the mouse is connected
-      mouse.move(0, (pitch.get() - oldPitch) * 100);//move mouse by current pitch (of accelerometer) - old pitch (of accelerometer)
-    }
-    oldPitch = pitch.get();//update old pitch
-    Serial.printf("Pitch: %f (%d)\n", pitch.get(), gPitch.stability());//prints accelerometer stability and (gyroscope stability)
+  gPitch.update(g.gyro.x);
+  if (gPitch.stability() > 8) {
+    if (mouse.isConnected()) mouse.move(0, (pitch.get() - oldPitch) * 100);
+    oldPitch = pitch.get();
+    Serial.printf("Pitch: %f (%d)\n", pitch.get(), gPitch.stability());
   }
-  if (gRoll.stability() > 8) {//currently using information from gyro
-
-    if (mouse.isConnected()){
-      mouse.move((roll.get() - oldRoll) * 100, 0);
-    }
+  if (gRoll.stability() > 8) {
+    if (mouse.isConnected()) mouse.move((roll.get() - oldRoll) * 100, 0);
     oldRoll = roll.get();
     Serial.printf("Roll: %f (%d)\n", roll.get(), gRoll.stability());
   }
-
   delay(10);
 }
