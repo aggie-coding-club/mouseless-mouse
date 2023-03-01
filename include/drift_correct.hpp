764673dad551fe9ef4fa77c97bf0088bc0d7ce4b
Juniper Mills <gblrfgr@gmail.com>
2023-03-01 16:14:25 -0600

    General quality-of-life changes
    :3

--- include/drift_correct.hpp
+++ include/drift_correct.hpp
@@ -1,23 +1,29 @@
+#ifndef MLM_DRIFT_CORRECT_HPP
+#define MLM_DRIFT_CORRECT_HPP
+
 #include <cstdint>
 
 /**
  * @class DriftCorrector
  * @brief Object offering drift-correction capabilities for real-valued signals
- * @details Uses a high-pass filter to correct for unintended long-term drift in a signal, assuming that changes below
- *  a minimum frequency are to be considered drift
+ * @details Uses a high-pass filter to correct for unintended long-term drift in a signal,
+ *          assuming that changes below a minimum frequency are to be considered drift
  */
 class DriftCorrector
 {
 private:
     float m_lastSampleUncorrected;
     float m_lastSampleCorrected;
-    float m_RC; // time constant (RC)
+    float m_timeSinceGoodSample; // to accurately keep track of time deltas even when samples
+                                 // need to be thrown out
+    float m_RC;                  // time constant (tau)
 
 public:
     /**
-     * @param cornerFrequency the maximum frequency (in Hz) of changes that should be considered drift
+     * @param cornerFrequency the maximum frequency (in Hz) of changes that should be
+     *                        considered drift
      */
-    DriftCorrector(float cornerFrequency = 0.1f);
+    DriftCorrector(float cornerFrequency = 0.1f) noexcept;
     /**
      * @brief Sends a value through for correction.
      *
@@ -25,5 +31,7 @@ public:
      * @param timeDelta the time (in seconds) since the last value was stored
      * @returns the value corrected for any drift that has been detected thus far.
      */
-    [[nodiscard]] float next(float value, float timeDelta);
+    [[nodiscard]] float next(float value, float timeDelta) noexcept;
 };
+
+#endif
--- src/drift_correct.cpp
+++ src/drift_correct.cpp
@@ -8,40 +8,48 @@ DriftCorrector::DriftCorrector(float cornerFrequency = 0.1f)
 {
     if (std::isnan(cornerFrequency) || std::isinf(cornerFrequency) || cornerFrequency <= 0.0f)
     {
-        Log.errorln("invalid corner frequency given to drift corrector, correcting to default 0.1 Hz");
+        Log.errorln(
+            "invalid corner frequency given to drift corrector, substituting with 0.1 Hz");
         cornerFrequency = 0.1f;
     }
 
     m_lastSampleCorrected = std::numeric_limits<float>::signaling_NaN();
     m_lastSampleUncorrected = std::numeric_limits<float>::signaling_NaN();
+    m_timeSinceGoodSample = 0.0f;
     m_RC = 1.0f / (2.0f * PI * cornerFrequency);
 }
 
-float DriftCorrector::next(float value, float timeDelta)
+float DriftCorrector::next(float sample, float timeDelta)
 {
     // Algorithm from https://en.wikipedia.org/wiki/High-pass_filter (2023-02-28)
 
-    if (std::isnan(value) || std::isinf(value))
+    if (std::isnan(sample) || std::isinf(sample))
     {
         Log.warningln("invalid sample passed into drift correction, ignoring");
-        return value;
+        if (!std::isnan(timeDelta) && !std::isinf(timeDelta) && timeDelta > 0.0f)
+        {
+            m_timeSinceGoodSample += timeDelta;
+        }
+        return sample;
     }
     else if (std::isnan(timeDelta) || std::isinf(timeDelta) || timeDelta <= 0.0f)
     {
         Log.warningln("invalid time delta passed into drift correction, ignoring");
-        return value;
+        return sample;
     }
+    timeDelta += m_timeSinceGoodSample;
+    m_timeSinceGoodSample = 0.0f;
 
     if (std::isnan(m_lastSampleCorrected) || std::isnan(m_lastSampleUncorrected))
     {
         // First sample taken in, just pass it straight through
-        m_lastSampleCorrected = value;
-        m_lastSampleUncorrected = value;
-        return value;
+        m_lastSampleCorrected = sample;
+        m_lastSampleUncorrected = sample;
+        return sample;
     }
     float alpha = m_RC / (m_RC + timeDelta);
-    float corrected = alpha * (m_lastSampleCorrected + value - m_lastSampleUncorrected);
+    float corrected = alpha * (m_lastSampleCorrected + sample - m_lastSampleUncorrected);
     m_lastSampleCorrected = corrected;
-    m_lastSampleUncorrected = value;
+    m_lastSampleUncorrected = sample;
     return corrected;
 }
