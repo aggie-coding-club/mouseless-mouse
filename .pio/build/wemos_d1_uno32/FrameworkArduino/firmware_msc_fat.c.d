78da01cae9d5ba8fc6e19527cec1d90d69af054f
Juniper Mills <gblrfgr@gmail.com>
2023-03-04 17:58:23 -0600

    Revert "Trying a gitignore change"
    HUH well turns out that didn't work whoopsie doodle

--- .pio/build/wemos_d1_uno32/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/wemos_d1_uno32/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/MattM/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- C:/Users/MattM/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
