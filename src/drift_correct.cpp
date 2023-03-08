f9e56e22238cecb507be89e787bbf77b85367f6b
Juniper Mills <gblrfgr@gmail.com>
2023-03-07 23:43:22 -0600

    Finished proof-of-concept ICM implementation
    Also reformatted a bunch of stuff! clang-format is so nice :3

--- src/drift_correct.cpp
+++ src/drift_correct.cpp
@@ -5,54 +5,44 @@
 #include <cmath>
 #include <limits>
 
-namespace mlm
-{
-    DriftCorrector::DriftCorrector(float timeDelta, float cornerFrequency) noexcept
-    {
-        if (!std::isfinite(cornerFrequency) || cornerFrequency <= 0.0f)
-        {
-            errors::doFatalError(
-                "invalid corner frequency passed into drift corrector", errors::SOFTWARE_INITIALIZATION_FAILED);
-        }
-        if (!std::isfinite(timeDelta) || timeDelta <= 0.0f)
-        {
-            errors::doFatalError(
-                "invalid time delta passed into drift corrector", errors::SOFTWARE_INITIALIZATION_FAILED);
-        }
+namespace mlm {
+DriftCorrector::DriftCorrector(float timeDelta, float cornerFrequency) noexcept {
+  if (!std::isfinite(cornerFrequency) || cornerFrequency <= 0.0f) {
+    errors::doFatalError("invalid corner frequency passed into drift corrector",
+                         errors::SOFTWARE_INITIALIZATION_FAILED);
+  }
+  if (!std::isfinite(timeDelta) || timeDelta <= 0.0f) {
+    errors::doFatalError("invalid time delta passed into drift corrector", errors::SOFTWARE_INITIALIZATION_FAILED);
+  }
 
-        m_lastSampleCorrected = std::numeric_limits<float>::signaling_NaN();
-        m_lastSampleUncorrected = std::numeric_limits<float>::signaling_NaN();
+  m_lastSampleCorrected = std::numeric_limits<float>::signaling_NaN();
+  m_lastSampleUncorrected = std::numeric_limits<float>::signaling_NaN();
 
-        float RC = 1.0f / (2.0f * PI * cornerFrequency);
-        m_alpha = RC / (RC + timeDelta);
-        if (!std::isfinite(m_alpha) || !std::isnormal(m_alpha))
-        {
-            errors::doFatalError(
-                "corner frequency given to drift corrector is too great or too small",
-                errors::SOFTWARE_INITIALIZATION_FAILED);
-        }
-    }
-
-    float DriftCorrector::next(float sample) noexcept
-    {
-        // Algorithm from https://en.wikipedia.org/wiki/High-pass_filter (2023-02-28)
-
-        if (!std::isfinite(sample))
-        {
-            Log.warningln("invalid sample passed into drift correction, ignoring");
-            return m_lastSampleCorrected;
-        }
-        if (std::isnan(m_lastSampleCorrected) || std::isnan(m_lastSampleUncorrected))
-        {
-            // First sample taken in, just pass it straight through
-            m_lastSampleCorrected = sample;
-            m_lastSampleUncorrected = sample;
-            return sample;
-        }
-
-        float corrected = m_alpha * (m_lastSampleCorrected + sample - m_lastSampleUncorrected);
-        m_lastSampleCorrected = corrected;
-        m_lastSampleUncorrected = sample;
-        return corrected;
-    }
+  float RC = 1.0f / (2.0f * PI * cornerFrequency);
+  m_alpha = RC / (RC + timeDelta);
+  if (!std::isfinite(m_alpha) || !std::isnormal(m_alpha)) {
+    errors::doFatalError("corner frequency given to drift corrector is too great or too small",
+                         errors::SOFTWARE_INITIALIZATION_FAILED);
+  }
 }
+
+float DriftCorrector::next(float sample) noexcept {
+  // Algorithm from https://en.wikipedia.org/wiki/High-pass_filter (2023-02-28)
+
+  if (!std::isfinite(sample)) {
+    Log.warningln("invalid sample passed into drift correction, ignoring");
+    return m_lastSampleCorrected;
+  }
+  if (std::isnan(m_lastSampleCorrected) || std::isnan(m_lastSampleUncorrected)) {
+    // First sample taken in, just pass it straight through
+    m_lastSampleCorrected = sample;
+    m_lastSampleUncorrected = sample;
+    return sample;
+  }
+
+  float corrected = m_alpha * (m_lastSampleCorrected + sample - m_lastSampleUncorrected);
+  m_lastSampleCorrected = corrected;
+  m_lastSampleUncorrected = sample;
+  return corrected;
+}
+} // namespace mlm
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,44 @@
 #include "errors.hpp"
 
 #include <Arduino.h>
-#include <Adafruit_MPU6050.h>
-#include <Wire.h>
-#include <BleMouse.h>
+#include <ArduinoEigen/Eigen/Geometry>
 #include <ArduinoLog.h>
-#include <array>
-#include <type_traits>
+#include <BleMouse.h>
+#include <Wire.h>
+
+#include <cmath>
+#include <limits>
 
 #define ICM_20948_USE_DMP
 #include <ICM_20948.h>
-#include <ArduinoEigen/Eigen/Geometry>
 
-#define BUTTON_PIN 14          // Which pin is the mouse click button connected to?
-#define ROLLING_BUFFER_SIZE 20 // How many inputs will we keep in rolling average array?
-
-Adafruit_MPU6050 mpu;                                // Initialize MPU - tracks rotation in terms of acceleration + gyroscope
-BleMouse mouse("Mouseless Mouse", "Espressif", 100); // Initialize bluetooth mouse to send mouse events
-
-[[nodiscard]] long sign(float inVal)
-{
-  return (inVal > 0) ? 1 : -1;
+/**
+ * @brief converts from a wider data type to a narrower one, capping the output value to prevent undefined behavior
+ *
+ * @param n the value to be converted
+ */
+template <typename Smaller, typename Larger> [[nodiscard]] Smaller saturate_cast(Larger n) noexcept {
+  if (n > std::numeric_limits<Smaller>::max()) {
+    return std::numeric_limits<Smaller>::max();
+  } else if (n < std::numeric_limits<Smaller>::min()) {
+    return std::numeric_limits<Smaller>::min();
+  } else {
+    return static_cast<Smaller>(n);
+  }
 }
 
 /**
  * @brief Transforms a given vector based on the orientation recorded by the DMP.
  *
- * @details Given a vector in Arduino-space (i.e., basis vectors relative to the ICM), uses the ICM sensor data to
- *   transform it into a vector in world-space (i.e., z parallel to gravity, y parallel to magnetic north, x
- *   parallel to magnetic east).
+ * @details Given a vector in Arduino-space (i.e., basis vectors relative to the ICM), uses the ICM
+ * sensor data to transform it into a vector in world-space (i.e., z parallel to gravity, y parallel
+ * to magnetic north, x parallel to magnetic east).
  *
  * @param vec the vector in Arduino-space to be transformed
  * @param packet the sensor data packet from the DMP
  * @pre the packet must contain valid orientation-mode 9-DOF Quat9 data.
  */
-[[nodiscard]] Eigen::Vector3<double>
-toWorldSpace(const Eigen::Vector3<double> vec, const icm_20948_DMP_data_t packet) noexcept
-{
+[[nodiscard]] Eigen::Vector3d toWorldSpace(const Eigen::Vector3d vec, const icm_20948_DMP_data_t packet) noexcept {
   const double scalingFactor = std::pow(2.0, 30.0);
   const double w = ((double)packet.Quat9.Data.Q1) / scalingFactor;
   const double x = ((double)packet.Quat9.Data.Q2) / scalingFactor;
@@ -47,180 +49,120 @@ toWorldSpace(const Eigen::Vector3<double> vec, const icm_20948_DMP_data_t packet
 }
 
 /**
- * Tracks the rolling average of a series of sampled floats.
- *
- * All samples are given equal weight. A maximum of buffer_length samples will be retained.
+ * @class Screen
+ * @brief Represents the viewscreen the mouse will be on
  */
-template <std::size_t buffer_length>
-class RollingAverage
-{
+class Screen {
 private:
-  std::array<float, buffer_length> m_buffer{0.0f};
-  std::size_t m_next = 0;
-  std::size_t m_num_values = 0;
-  long m_direction_stability = 0;
-  float m_average = 0.0f;
+  Eigen::Vector3d m_center;
+  Eigen::Vector3d m_forward;
+  Eigen::Vector3d m_right;
+  Eigen::Vector3d m_up;
 
 public:
-  // Guarantee stability() will never overflow
-  static_assert(buffer_length < std::numeric_limits<long>::max(),
-                "buffer_length must be shorter than the maximum magnitude of a long");
+  /**
+   * @param sensitivity the factor by which changes in the mouse angle are multiplied
+   * @param forward a vector normal to the plane of the screen and pointing out the back
+   * @param up a vector parallel to the plane of the screen and pointing straight up it
+   */
+  Screen(double sensitivity, Eigen::Vector3d forward, Eigen::Vector3d up) noexcept
+      : m_center(forward.normalized() * sensitivity), m_forward(forward.normalized()), m_up(up.normalized()),
+        m_right(m_forward.cross(m_up)) {}
 
   /**
-   * Adds another value to the rolling average buffer.
+   * @brief recalibrate the position of the screen in space
    *
-   * Also updates the running average and directional stability measurements. If the buffer is full, val will
-   * replace the oldest value in the buffer.
-   *
-   * @param val the value to be added
-   * @pre The magnitude of val must be less than the quotient of the greatest finite float value divided by
-   *  buffer_length.
+   * @param sensitivity factor by which changed in the mouse angle are multiplied
+   * @param forward a vector normal to the plane of the screen and pointing out the back
+   * @param up a vector parallel to the plane of the screen and pointing straight up it
    */
-  void update(float val)
-  {
-    if (m_num_values > 0)
-    {
-      float preceding = (m_next == 0) ? m_buffer[buffer_length - 1] : m_buffer[m_next - 1];
-      if (m_num_values == buffer_length)
-      {
-        // Only roll back stability if we're replacing a value, not just adding one
-        m_direction_stability -= sign(m_buffer[m_next] - preceding);
-      }
-      m_direction_stability += sign(val - preceding);
-    }
-
-    float overall_sum = m_average * m_num_values;
-    overall_sum -= m_buffer[m_next]; // Depends on buffer being 0.0f-initialized
-    overall_sum += val;
-
-    if (m_num_values < buffer_length)
-    {
-      m_num_values++;
-    }
-    m_average = overall_sum / m_num_values;
-    m_buffer[m_next] = val;
-
-    m_next++;
-    m_next %= buffer_length;
+  void recalibrate(double sensitivity, Eigen::Vector3d forward, Eigen::Vector3d up) noexcept {
+    m_center = forward.normalized() * sensitivity;
+    m_forward = forward.normalized();
+    m_up = up.normalized();
+    m_right = m_forward.cross(m_up);
   }
 
   /**
-   * Measures the "stability" of the samples over time.
+   * @brief determines a pixel position the 'mouse' is pointing at
    *
-   * The stability is measured as an unsigned long integer whose magnitude depends on the length of the buffer. If the
-   * samples are uniformly increasing or decreasing, then the stability will be equal to the length of the buffer.
-   * Alternatively, if the samples are alternating up and down, stability will be measured as zero.
+   * @details The screen is treated as being `sensitivity` pixels away from the mouse. If the mouse is facing away
+   * from the screen, an imaginary vector is drawn backwards from the mouse and calculations are performed using
+   * this. If the mouse is exactly parallel with the screen, behavior is undefined. Pixel positions are given
+   * relative to the center of the screen and should not be treated as absolute.
+   * @param mouseDirection a unit vector denoting the 'forward' vector of the mouse
    */
-  [[nodiscard]] long stability() const
-  {
-    return std::abs(m_direction_stability);
-  }
-
-  /**
-   * Gets the mean of the last buffer_length samples.
-   *
-   * Returns zero if no samples have been added. If less than buffer_length samples have been added, then returns
-   * the average of all samples taken.
-   */
-  [[nodiscard]] float get() const noexcept
-  {
-    return m_average;
+  [[nodiscard]] Eigen::Vector2<long> project(const Eigen::Vector3d mouseDirection) const noexcept {
+    const Eigen::Hyperplane<double, 3> screenPlane(m_forward, m_center);
+    const Eigen::ParametrizedLine<double, 3> ray(Eigen::Vector3d::Zero(), mouseDirection);
+    const Eigen::Vector3d intersectionPoint = ray.intersectionPoint(screenPlane);
+    return Eigen::Vector2<long>((long)m_right.dot(intersectionPoint - m_center),
+                                (long)m_up.dot(intersectionPoint - m_center));
   }
 };
 
-volatile bool buttonPress = false; // Is the button currently pressed?
+ICM_20948_I2C icm;
+BleMouse mouse("Mouseless Mouse", "Espressif"); // Initialize Bluetooth mouse object to send mouse events
+Screen screen(100, Eigen::Vector3d(0, 1, 0), Eigen::Vector3d(0, 0, 1));
 
-// Initialize values to be smoothed later on
-RollingAverage<ROLLING_BUFFER_SIZE> roll;
-RollingAverage<ROLLING_BUFFER_SIZE> pitch;
-RollingAverage<ROLLING_BUFFER_SIZE> gRoll;
-RollingAverage<ROLLING_BUFFER_SIZE> gPitch;
+volatile bool buttonPress = false;
+void IRAM_ATTR onButtonPress() { buttonPress = true; }
 
-// set previous inputs to 0
-float oldRoll = 0;
-float oldPitch = 0;
+Eigen::Vector2<long> mousePosition;
 
-// sets up a function that can interupt and immediately be ran
-void IRAM_ATTR onButtonPress()
-{
-  buttonPress = true;
-}
-
-void setup()
-{
+void setup() {
+  constexpr uint8_t BUTTON_PIN = 14;
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(BUTTON_PIN, onButtonPress, FALLING); // when Button_pin goes from High to low (button is released) onButtonPress is ran
+  attachInterrupt(BUTTON_PIN, onButtonPress, FALLING);
 
   // starts Serial Monitor
   Serial.begin(115200);
   Log.begin(LOG_LEVEL_VERBOSE, &Serial);
 
-  if (!mpu.begin())
-  {
-    mlm::errors::doFatalError("no MPU detected", mlm::errors::HARDWARE_INITIALIZATION_FAILED);
+  if (!icm.begin() || icm.initializeDMP() != ICM_20948_Stat_Ok ||
+      icm.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) != ICM_20948_Stat_Ok ||
+      icm.setDMPODRrate(DMP_ODR_Reg_Quat9, 0) != ICM_20948_Stat_Ok || icm.enableFIFO() != ICM_20948_Stat_Ok ||
+      icm.enableDMP() != ICM_20948_Stat_Ok || icm.resetFIFO() != ICM_20948_Stat_Ok ||
+      icm.resetDMP() != ICM_20948_Stat_Ok) {
+    mlm::errors::doFatalError("ICM initialization failed", mlm::errors::HARDWARE_INITIALIZATION_FAILED);
+  } else {
+    Log.infoln("ICM successfully initialized");
   }
-  else
-  {
-    Log.infoln("Found MPU6050");
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    Log.infoln("Accelerometer range set to +-8 G");
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    Log.infoln("Gyrometer range set to +-500 deg");
-    mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
-    Log.infoln("Low-pass filter frequency set to 44 Hz");
-    mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ); // This choice of value might be problematic, needs testing lol
-    Log.infoln("High-pass filter frequency set to 0.63 Hz");
+
+  icm_20948_DMP_data_t data_frame;
+  for (;;) {
+    icm.readDMPdataFromFIFO(&data_frame);
+    if (icm.status != ICM_20948_Stat_Ok && icm.status != ICM_20948_Stat_FIFOMoreDataAvail) {
+      Log.traceln("Waiting for first DMP packet");
+      delay(20);
+    } else {
+      Log.traceln("First DMP packet received");
+      mousePosition = screen.project(toWorldSpace(Eigen::Vector3d(0, 1, 0), data_frame));
+      break;
+    }
   }
 
   mouse.begin();
-  Log.infoln("Started Bluetooth mouse");
+  Log.infoln("Bluetooth mouse successfully initialized");
 }
 
-void loop()
-{
-  sensors_event_t a, g, temp; // sets events (special class)
-  mpu.getEvent(&a, &g, &temp);
-
-  // button press stuff
-  if (buttonPress)
-  {
-    if (mouse.isConnected())
-    {
+void loop() {
+  if (buttonPress) {
+    if (mouse.isConnected()) {
       mouse.click();
-    }
-    else
-    {
+    } else {
       Log.noticeln("Mouse not connected!");
     }
     buttonPress = false;
   }
 
-  roll.update(atan2(a.acceleration.z, a.acceleration.x)); // weird math to calculate direction change for roll (currently x-axis) then add it to array of prior inputs
-  pitch.update(atan2(a.acceleration.z, a.acceleration.y));
-  gRoll.update(g.gyro.y);
-  gPitch.update(g.gyro.x); // adding gyroscope in x axis
-
-  // currently using information from gyroscope for if statements
-  if (gPitch.stability() > 8)
-  { // if the stability is decent
-
-    if (mouse.isConnected())
-    {                                                // If the mouse is connected
-      mouse.move(0, (pitch.get() - oldPitch) * 100); // move mouse by current pitch (of accelerometer) - old pitch (of accelerometer)
-    }
-    oldPitch = pitch.get();                                                          // update old pitch
-    Log.trace("Pitch: %f rad (stability of %d)\n", pitch.get(), gPitch.stability()); // prints accelerometer stability and (gyroscope stability)
+  icm_20948_DMP_data_t data_frame;
+  icm.readDMPdataFromFIFO(&data_frame);
+  if (icm.status == ICM_20948_Stat_Ok || icm.status == ICM_20948_Stat_FIFOMoreDataAvail) {
+    Eigen::Vector2<long> diff = screen.project(toWorldSpace(Eigen::Vector3d(0, 1, 0), data_frame)) - mousePosition;
+    mouse.move(saturate_cast<signed char>(diff.x()), saturate_cast<signed char>(diff.y()));
   }
-  if (gRoll.stability() > 8)
-  { // currently using information from gyro
-
-    if (mouse.isConnected())
-    {
-      mouse.move((roll.get() - oldRoll) * 100, 0);
-    }
-    oldRoll = roll.get();
-    Log.trace("Roll: %f rad (stability of %d)\n", roll.get(), gRoll.stability());
+  if (icm.status != ICM_20948_Stat_FIFOMoreDataAvail) {
+    delay(10);
   }
-
-  delay(10);
 }
