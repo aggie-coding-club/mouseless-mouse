99ad25ca5ca8c493612804dddf43dafcd8065d35
Juniper Mills <gblrfgr@gmail.com>
2023-04-07 17:27:28 -0500

    The Merge
    Just getting everything knit in. We'll work functionality soon.

--- ulp/ulp_main.common.d
+++ ulp/ulp_main.common.d
@@ -1,5 +1,5 @@
 ulp_main.common.ld: \
- C:\Users\jeury\mouseless-mouse2\.pio\libdeps\ttgo-lora32-v1\ulptool-pio/src/ld\esp32.ulp.ld \
- C:\Users\jeury\mouseless-mouse2\.pio\libdeps\ttgo-lora32-v1\ulptool-pio\src\include\ulptool/sdkconfig.h
+ c:\Users\MattM\Documents\GitHub\mouseless-mouse\.pio\libdeps\ttgo-lora32-v1\ulptool-pio/src/ld\esp32.ulp.ld \
+ C:\Users\MattM\.platformio\packages\framework-arduinoespressif32\tools\sdk\esp32\dio_qspi\include/sdkconfig.h
 
-C:\Users\jeury\mouseless-mouse2\.pio\libdeps\ttgo-lora32-v1\ulptool-pio\src\include\ulptool/sdkconfig.h:
+C:\Users\MattM\.platformio\packages\framework-arduinoespressif32\tools\sdk\esp32\dio_qspi\include/sdkconfig.h:
