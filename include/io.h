ec6f44ce10592a5e023cd0fea804621b360f4c39
Juniper Mills <gblrfgr@gmail.com>
2023-04-07 19:07:19 -0500

    Reformatted main source files

--- include/io.h
+++ include/io.h
@@ -1,9 +1,9 @@
 #ifndef IO_HANDLERS
 #define IO_HANDLERS
 
-#include <Arduino.h>
 #include "display.h"
 #include "mouse.h"
+#include <Arduino.h>
 
 /*
  * Button debouncing proceeds as follows:
@@ -17,17 +17,19 @@
  *     If not, set new button state and restart the timer
  */
 
-#define DEBOUNCE_TIME 50            // Debouncing interval in milliseconds
-#define LONGPRESS_TIME 300          // Buttons held longer than LONGPRESS_TIME milliseconds will fire holdEvent rather than bumpEvent on release
+#define DEBOUNCE_TIME 50 // Debouncing interval in milliseconds
+#define LONGPRESS_TIME                                                                                                 \
+  300 // Buttons held longer than LONGPRESS_TIME milliseconds will fire holdEvent rather than bumpEvent on release
 
-#define TOUCH_DEFAULT_THRESHOLD 40  // Threshold value below which touch counter values are interpreted as touches
-#define TOUCH_HYSTERESIS 20         // Once a touch pad is in the "pressed" state, it must go this far above the threshold to be "released"
-#define TOUCH_POLL_RATE 5           // When a touch is recorded, the pads will be polled every TOUCH_POLL_RATE ms
+#define TOUCH_DEFAULT_THRESHOLD 40 // Threshold value below which touch counter values are interpreted as touches
+#define TOUCH_HYSTERESIS                                                                                               \
+  20 // Once a touch pad is in the "pressed" state, it must go this far above the threshold to be "released"
+#define TOUCH_POLL_RATE 5 // When a touch is recorded, the pads will be polled every TOUCH_POLL_RATE ms
 
 // Button class - wrapper for I/O buttons with debouncing, timestamping, and event generation
 class Button {
 private:
-  static void buttonISR(void* instancePtr);
+  static void buttonISR(void *instancePtr);
   static void finalDebounce(xTimerHandle timer);
 
 public:
@@ -38,8 +40,8 @@ public:
   pageEvent_t holdEvent;
   xTimerHandle debounceTimer;
   bool isPressed;
-  uint32_t pressTimestamp;    // Timestamp of the last time the button was pressed
-  uint32_t releaseTimestamp;  // Timestamp of the last time the button was released
+  uint32_t pressTimestamp;   // Timestamp of the last time the button was pressed
+  uint32_t releaseTimestamp; // Timestamp of the last time the button was released
   uint32_t stateChangeTimestamp;
 
   Button(byte pin, xQueueHandle eventQueue, pageEvent_t pressEvent, pageEvent_t bumpEvent, pageEvent_t holdEvent);
@@ -50,14 +52,12 @@ public:
 class TouchPadInstance;
 
 // By default, no TouchPadInstance instances have been defined
-static TouchPadInstance* touchInstanceMap[10] = {
-  nullptr, nullptr, nullptr, nullptr, nullptr,
-  nullptr, nullptr, nullptr, nullptr, nullptr
-};
+static TouchPadInstance *touchInstanceMap[10] = {nullptr, nullptr, nullptr, nullptr, nullptr,
+                                                 nullptr, nullptr, nullptr, nullptr, nullptr};
 
 class TouchPadInstance {
 private:
-  static void touchISR(void* instancePtr);
+  static void touchISR(void *instancePtr);
   static void touchPoll(xTimerHandle timer);
   static xTimerHandle pollTimer;
 
@@ -72,7 +72,8 @@ public:
   uint32_t releaseTimestamp;
   uint32_t stateChangeTimestamp;
 
-  TouchPadInstance(byte touchPadNum, xQueueHandle eventQueue, mouseEvent_t pressEvent, mouseEvent_t releaseEvent, byte pin);
+  TouchPadInstance(byte touchPadNum, xQueueHandle eventQueue, mouseEvent_t pressEvent, mouseEvent_t releaseEvent,
+                   byte pin);
   void attach();
   void setThreshold(uint16_t touchThreshold);
   bool remapEvents(mouseEvent_t pressEvent, mouseEvent_t releaseEvent);
