30fbded936fc0bdee80f05535b60927136d1cc27
Juniper Mills <gblrfgr@gmail.com>
2023-04-12 21:07:43 -0500

    Adjustments to sensitivity

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@
 #define SCROLL_TOUCH_CHANNEL 3
 #define LOCK_TOUCH_CHANNEL 4
 #define CALIBRATE_TOUCH_CHANNEL 2
-constexpr signed char SENSITIVITY = 8;
+constexpr signed char SENSITIVITY = 12;
 
 // Mouse logic globals
 ICM_20948_I2C icm;
@@ -341,7 +341,7 @@ void loop() {
   if (icm.dataReady() && mouseEnableState) {
     icm.getAGMT();
     Eigen::Vector3f posY = mouseSpaceToWorldSpace(Eigen::Vector3f{0.0f, 1.0f, 0.0f}, icm);
-    signed char xMovement = normalizeMouseMovement(posY.dot(calibratedPosX)) * SENSITIVITY;
+    signed char xMovement = normalizeMouseMovement(posY.dot(calibratedPosX)) * 1.5f * SENSITIVITY;
     Serial.printf("%i\n", xMovement);
     signed char zMovement = normalizeMouseMovement(posY.dot(calibratedPosZ)) * SENSITIVITY;
     if (!scrollEnableState) {
