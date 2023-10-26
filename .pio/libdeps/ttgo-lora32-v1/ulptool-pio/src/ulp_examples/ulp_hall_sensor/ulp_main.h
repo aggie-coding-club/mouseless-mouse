703efafbd919b884a8a3722a9c6c748a5f83ef44
TheEnderChipmunk <amruthtech2004@gmail.com>
2023-10-26 16:29:11 -0500

    Clean-up repo

--- .pio/libdeps/ttgo-lora32-v1/ulptool-pio/src/ulp_examples/ulp_hall_sensor/ulp_main.h
+++
@@ -1,7 +0,0 @@
-#include "Arduino.h"
-
-extern uint32_t ulp_entry;
-extern uint32_t ulp_Sens_Vp0;
-extern uint32_t ulp_Sens_Vn0;
-extern uint32_t ulp_Sens_Vp1;
-extern uint32_t ulp_Sens_Vn1;
--- ulp/ulp_main.common.d
+++
@@ -1,5 +0,0 @@
-ulp_main.common.ld: \
- C:\Users\amrut\Desktop\My\ Stuff\TAMU\ Stuff\Aggie\ Coding\ Club\Mouseless\ Mouse\mouseless-mouse\.pio\libdeps\ttgo-lora32-v1\ulptool-pio/src/ld\esp32.ulp.ld \
- C:\Users\amrut\Desktop\My\ Stuff\TAMU\ Stuff\Aggie\ Coding\ Club\Mouseless\ Mouse\mouseless-mouse\.pio\libdeps\ttgo-lora32-v1\ulptool-pio\src\include\ulptool/sdkconfig.h
-
-C:\Users\amrut\Desktop\My\ Stuff\TAMU\ Stuff\Aggie\ Coding\ Club\Mouseless\ Mouse\mouseless-mouse\.pio\libdeps\ttgo-lora32-v1\ulptool-pio\src\include\ulptool/sdkconfig.h:
