8d38f6b18008f3364af70c0ab5319a3dfb52f803
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2023-11-05 09:04:01 -0600

    Added error handling to 3ml_parser.cpp

--- src/3ml_parser.cpp
+++ src/3ml_parser.cpp
@@ -1,5 +1,5 @@
 #include "3ml_parser.h"
-#include <Arduino.h>
+#include "3ml_error.h"
 #include <cstring>
 #include <string>
 #include <vector>
@@ -19,15 +19,26 @@ Attribute parse_attribute(const char *&cursor) {
     cursor++;
   }
   name = std::string(name_begin, cursor - name_begin);
+  maybe_error(name.empty(), "invalid attribute name");
 
+  bool has_seen_equals_sign = false;
   while (std::isspace(*cursor) || *cursor == '=') {
+    maybe_error(has_seen_equals_sign && *cursor == '=', "only one equals sign may be in an attribute");
+    maybe_error(*cursor == '\n', "attributes must be on one line");
+    if (*cursor == '=') {
+      has_seen_equals_sign = true;
+    }
     cursor++;
   }
+  maybe_error(*cursor != '"', "expected string");
   cursor++;
   const char *value_begin = cursor;
-  while (*cursor != '"') {
+  while (*cursor != '"' && *cursor != '\0') {
+    maybe_error(!std::isprint(*cursor), "unprintable character in string");
+    maybe_error(*cursor == '\n', "expected closing quote, found newline");
     cursor++;
   }
+  maybe_error(*cursor == '\0', "expected closing quote, found EOF");
   value = remove_escape_codes(std::string(value_begin, cursor - value_begin));
 
   return Attribute(name, value);
@@ -39,12 +50,13 @@ Tag parse_tag(const char *&cursor) {
   bool is_closing = false;
   bool is_self_closing = false;
 
-  cursor++;
+  cursor++; // always starts on a '<', so we can skip it
   if (*cursor == '/') {
     is_closing = true;
     cursor++;
   }
   while (std::isspace(*cursor)) {
+    maybe_error(*cursor == '\n', "did not expect a newline");
     cursor++;
   }
 
@@ -54,15 +66,18 @@ Tag parse_tag(const char *&cursor) {
   }
   name = std::string(name_begin, cursor - name_begin);
 
-  while (std::isalpha(*cursor)) {
+  while (std::isspace(*cursor)) {
+    maybe_error(*cursor == '\n', "did not expect a newline");
     cursor++;
   }
-  while (*cursor != '/' && *cursor != '>') {
+  while (*cursor != '/' && *cursor != '>' && *cursor != '\0') {
     attributes.push_back(parse_attribute(cursor));
     while (std::isspace(*cursor)) {
+      maybe_error(*cursor == '\n', "did not expect a newline");
       cursor++;
     }
   }
+  maybe_error(*cursor == '\0', "unclosed tag");
 
   if (*cursor == '/') {
     is_self_closing = true;
@@ -70,22 +85,36 @@ Tag parse_tag(const char *&cursor) {
   }
   cursor++;
 
+  maybe_error(is_closing && is_self_closing, "tag cannot be both closing and self-closing");
+  maybe_error(is_closing && !attributes.empty(), "closing tags cannot have attributes");
+
   return Tag(name, attributes, is_closing, is_self_closing);
 }
 
 std::string remove_escape_codes(std::string escaped) {
   std::string result;
   bool in_escape_code = false;
+  bool expect_hashtag = false;
   unsigned int current_escape_num = 0;
   for (auto c : escaped) {
     if (!in_escape_code && c == '&') {
       in_escape_code = true;
+      expect_hashtag = true;
+    } else if (in_escape_code && c == '#') {
+      maybe_error(!expect_hashtag, "invalid escape code");
+      expect_hashtag = false;
     } else if (in_escape_code && '0' <= c && c <= '9') {
+      maybe_error(expect_hashtag, "invalid escape code");
       current_escape_num *= 10;
       current_escape_num += c - '0';
+      maybe_error(current_escape_num >= 256, "escape code not valid ASCII");
     } else if (in_escape_code && c == ';') {
+      maybe_error(!std::isprint(current_escape_num), "unprintable escape code");
+      maybe_error(expect_hashtag, "empty escape code");
       in_escape_code = false;
       result.push_back(static_cast<char>(current_escape_num));
+    } else if (in_escape_code) {
+      maybe_error(true, "invalid character in escape code");
     } else if (!in_escape_code) {
       result.push_back(c);
     }
@@ -116,10 +145,11 @@ DirtyDOM parse_string(const char *str) {
     if (is_normal_mode) {
       const char *plaintext_begin = cursor;
       while (*cursor != '<' && *cursor != '\0') {
+        maybe_error(!std::isprint(*cursor), "unprintable character in plaintext");
         cursor++;
       }
-      parse_chain.push_back(
-          ParseNode(Plaintext(trim(remove_escape_codes(std::string(plaintext_begin, cursor - plaintext_begin))))));
+      std::string processed = trim(remove_escape_codes(std::string(plaintext_begin, cursor - plaintext_begin)));
+      parse_chain.push_back(ParseNode(Plaintext(processed)));
       is_normal_mode = false;
     } else {
       parse_chain.push_back(ParseNode(parse_tag(cursor)));
@@ -142,6 +172,7 @@ DirtyDOM parse_string(const char *str) {
       tag_stack.push_back(tag_node);
     } else if (node.is_tag && node.tag.is_closing) {
       DirtyDOMTagNode current = tag_stack.back();
+      maybe_error(current.tag_name != node.tag.name, "closing tags must match the opening tag");
       tag_stack.pop_back();
       if (tag_stack.empty()) {
         top_level_nodes.push_back(DirtyDOMNode(current));
