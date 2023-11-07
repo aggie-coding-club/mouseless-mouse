c09b3fd0855335d1311d2e5672f9fbb466be437d
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2023-11-07 16:39:49 -0600

    Began adding scripting logic to DOM

--- include/3ml_cleaner.h
+++ include/3ml_cleaner.h
@@ -9,6 +9,35 @@ namespace threeml {
 
 enum class NodeType { PLAINTEXT, TITLE, DIV, HEAD, BODY, SCRIPT, H1, A, BUTTON, INPUT_NODE };
 
+using PlaintextData = std::string;
+struct ButtonData {
+  std::string label;
+  std::string callback;
+
+  ButtonData(std::string label, std::string callback) : label(label), callback(callback) {}
+};
+struct LinkData {
+  std::string label;
+  std::string points_to;
+
+  LinkData(std::string label, std::string points_to) : label(label), points_to(points_to) {}
+};
+struct TextInputData {
+  std::string contents;
+  std::string oninput_callback;
+
+  TextInputData(std::string oninput_callback = "") : contents(""), oninput_callback(oninput_callback) {}
+};
+struct SliderData {
+  std::size_t min;
+  std::size_t max;
+  std::size_t value;
+  std::string oninput_callback;
+
+  SliderData(std::size_t min, std::size_t max, std::string oninput_callback = "")
+      : min(min), max(max), value(min), oninput_callback(oninput_callback) {}
+};
+
 struct DOMNode {
   NodeType type;
   std::string plaintext_content;
