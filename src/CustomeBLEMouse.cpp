a2958f50f8a2e536c86e624be1708c49ba4d78bc
Lapras <jermurray2@gmail.com>
2023-11-26 17:45:10 -0600

    code that actually compiles ??? I forgot how to c++ .h files work

--- src/CustomeBLEMouse.cpp
+++ src/CustomeBLEMouse.cpp
@@ -1,20 +1,16 @@
+#include "CustomBLEMouse.h"
 #include <BleMouse.h>
 #include <BLEDevice.h>
 
-class CustomBLEMouse : public BleMouse {
-    private:
-        TaskHandle_t handle;
 
-    public:
-    CustomBLEMouse(std::string deviceName, std::string deviceManufacturer) : BleMouse(deviceName, deviceManufacturer, 100) {};
+CustomBLEMouse::CustomBLEMouse(std::string deviceName, std::string deviceManufacturer) : BleMouse(deviceName, deviceManufacturer, 100) {};
 
-    void begin() {
-        BleMouse::begin();
-        handle = xTaskGetHandle("server");
-    }
+void CustomBLEMouse::begin() {
+    BleMouse::begin();
+    handle = xTaskGetHandle("server");
+}
 
-    void end() {
-        vTaskDelete(handle);
-        BLEDevice::deinit(true);
-    }
-};
+void CustomBLEMouse::end() {
+    vTaskDelete(handle);
+    BLEDevice::deinit(true);
+}
